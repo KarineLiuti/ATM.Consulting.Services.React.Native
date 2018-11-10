2c8d096237a05c6a53b8868c011fa1a665227d95
Karine Liuti <kah.liuti@gmail.com>
2018-11-10 10:20:55 -0200

    Made default scene layout

--- src/components/DefaultScene.js
+++ src/components/DefaultScene.js
@@ -2,17 +2,33 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   View,
-  StatusBar
+  StatusBar,
+  Image
 } from 'react-native';
 import ToolBar from './ToolBar';
+
+const logo = require('./../imgs/logo.png');
+const menuCliente = require('./../imgs/menu_cliente.png');
+const menuContato = require('./../imgs/menu_contato.png');
+const menuEmpresa = require('./../imgs/menu_empresa.png');
+const menuServico = require('./../imgs/menu_servico.png');
 
 export default class DefaultScene extends Component {
   render() {
     return (
       <View>
         <StatusBar backgroundColor="#CCC" />
-        <View>
-          <ToolBar />
+        <ToolBar />
+        <View style={styles.logo}>
+          <Image source={logo} />
+        </View>
+        <View style={styles.menuGroup}>
+          <Image style={styles.imageMenu} source={menuCliente} />
+          <Image style={styles.imageMenu} source={menuContato} />
+        </View>
+        <View style={styles.menuGroup}>
+          <Image style={styles.imageMenu} source={menuEmpresa} />
+          <Image style={styles.imageMenu} source={menuServico} />
         </View>
       </View>
     );
@@ -20,5 +36,16 @@ export default class DefaultScene extends Component {
 }
 
 const styles = StyleSheet.create({
+  logo: {
+    marginTop: 30,
+    alignItems: 'center'
+  },
+  menuGroup: {
+    flexDirection: 'row',
+    justifyContent: 'center'
+  },
+  imageMenu: {
+    margin: 15
+  }
 });
 
